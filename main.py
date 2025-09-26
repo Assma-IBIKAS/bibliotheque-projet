fbefbb7d4d4622b7ae47fb40b087b2f39c866ebb
maryem <maryemelbergui@gmail.com>
2025-09-26 17:11:38 +0100

    menue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from users import get_majeurs, mjsc,resume
-
+from books import affichage_d_etulisateur,trier_livres, afficher_un_dictionnair, identifien_ancienne_recent
 if __name__ == "__main__":
 
     majeurs = get_majeurs()
@@ -9,8 +9,37 @@ if __name__ == "__main__":
     print("Noms en majuscules :")
     print(mjsc(majeurs))
     print()
-#jgjfkdkd
+
     # afficher les résumés
     print("Résumé par utilisateur :")
     for r in resume(majeurs):
         print(r)
+while True:
+    choice= input("choose an operation \n 1.affichage des utilisateur \n 2.afficher les j'aime de chaque livre \n 3. afficher les livres\n 4.Livres le plus ancien et le plus récent \n 5.Filtrer les utilisateurs majeurs \n 6.Noms en majuscules \n 7. \n 8. \n 9.quitter ")
+    match choice :
+          case "1":
+              print(affichage_d_etulisateur())
+              continue
+          case "2":
+              print(afficher_un_dictionnair())
+              continue
+          case "3":
+              print(trier_livres())
+              continue
+          case "4":
+              print( identifien_ancienne_recent())
+              continue
+          case "5":
+                majeurs = get_majeurs()
+                print("Utilisateurs majeurs : ",majeurs)
+                continue
+          case "6":
+                 print("Noms en majuscules :")
+                 print(mjsc(majeurs))
+                 print()
+                 continue
+          case "9":
+            break
+          case _:
+            continue
+
