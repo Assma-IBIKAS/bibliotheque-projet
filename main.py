1b24dd88c49583657ea00391f1d30968be6389de
Assma-IBIKAS <Assma-IBIKAS>
2025-09-26 12:21:17 +0100

    last qst

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from users import get_majeurs, mjsc
+from users import get_majeurs, mjsc,resume
 
 if __name__ == "__main__":
 
-    print(get_majeurs())
+    majeurs = get_majeurs()
+    print("Utilisateurs majeurs : ",majeurs)
 
     # Afficher les noms formatés en majuscules
     print("Noms en majuscules :")
-    print(mjsc())
+    print(mjsc(majeurs))
     print()
 
-    
-   
+    # afficher les résumés
+    print("Résumé par utilisateur :")
+    for r in resume(majeurs):
+        print(r)
--- users.py
+++ users.py
@@ -6,5 +6,20 @@ def get_majeurs():
     return list(filter(lambda u:u[3] >= 18, utilisateurs))
 
 #2- formater les noms en mjs : 
-def mjsc():
+def mjsc(users):
     return list(map(lambda u: f"{u[1].upper()} {u[2].upper()}", utilisateurs))
+
+#3- creer un dictionnaire :
+
+def livres_utilisateur(user_id):
+    return [titre for (uid, titre) in aime_livres if uid == user_id]
+
+
+def resume(users):
+    resumes = []
+    for u in users:
+        id_user, prenom, nom, age = u
+        livres = livres_utilisateur(id_user)
+        livres_str = ", ".join([f"'{titre}'" for titre in livres])
+        resumes.append(f"{prenom.upper()} {nom.upper()} ({age} ans) aime : {livres_str}")
+    return resumes
