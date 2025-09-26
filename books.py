92fcef9a12574f738c824b1a5d669bdbc48b3a43
maryem <maryemelbergui@gmail.com>
2025-09-26 12:02:04 +0100

    question2

--- books.py
+++ books.py
@@ -1,5 +1,13 @@
 import data as d
 #Trier les livres par année de publication (sort))
 books= list(d.livres)
-new_list =sorted(books, key=lambda x: x["année"])
-print(new_list)
+def trier_livres():
+     global books
+     new_list =sorted(books, key=lambda x: x["année"])
+     return new_list
+
+def identifien_ancienne_recent():
+     livre_trier= trier_livres()
+     ancien_livre= livre_trier[0]
+     livre_recent= livre_trier[-1]
+     print (f" le plus ancien:  {ancien_livre} \n le plus récent: {livre_recent}")
